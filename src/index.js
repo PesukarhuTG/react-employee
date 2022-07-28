f72660bcd6076bd54e65887c6106d73302301b11
Tatiana <ganzhinovat@yandex.ru>
2022-07-28 10:46:21 +0300

    feat: add app.js

--- src/index.js
+++ src/index.js
@@ -1,21 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-//import App from './App';
+import App from './components/app/app';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-//const elem = <h2>Hello, yoy</h2>;
-//const elem = React.createElement('h2', { className: 'greet'}, 'Hello');
-//const text = new Date();
-const elem = (
-  <div>
-    <h2>Hello: {[1,2,3]}</h2>
-    <input type="text" />
-    <button />
-  </div>
-
-);
 
 root.render(
-  elem
+  <App />
 );
