2d3a908d2ec9816dff1e82520451d3b8afc52acc
Tatiana <ganzhinovat@yandex.ru>
2022-07-28 15:38:52 +0300

    feat: add state to component

--- src/components/employeesListItem/employeesListItem.js
+++ src/components/employeesListItem/employeesListItem.js
@@ -1,28 +1,59 @@
 import './employeesListItem.css';
+import { Component } from 'react';
 
-const EmployeesListItem = ( {name, salary, increase} ) => {
-    const classNames = increase 
-          ? 'list-group-item d-flex justify-content-between increase'
-          : 'list-group-item d-flex justify-content-between';
+class EmployeesListItem extends Component {
+    constructor(props) {
+      super(props);
 
-    return (
-        <li className={classNames}>
-            <span className="list-group-item-label">{name}</span>
-            <input type="text" className="list-group-item-input" defaultValue={salary + '$'}/>
-            <div className='d-flex justify-content-center align-items-center'>
-                <button type="button"
-                    className="btn-cookie btn-sm ">
-                    <i className="fas fa-cookie"></i>
-                </button>
+      this.state = {
+        increase: false,
+        like: false,
+      }
+    }
 
-                <button type="button"
-                        className="btn-trash btn-sm ">
-                    <i className="fas fa-trash"></i>
-                </button>
-                <i className="fas fa-star"></i>
-            </div>
-        </li>
-    )
+    onIncrease = () => {
+      //чтобы не писать state.increase
+      this.setState(({increase}) => ({
+        //внутри объекта устанавливаем новое св-во increase противополложное предыдущему
+        increase: !increase
+      }))
+    }
+
+    toggleLike = () => {
+      this.setState(({like}) => ({
+        like: !like
+      }))
+
+    }
+
+    render() {
+      const {name, salary} = this.props;
+      const {increase, like} = this.state;
+
+      let classNames = 'list-group-item d-flex justify-content-between';
+      if (increase) classNames += ' increase';
+      if (like) classNames += ' like';
+
+      return (
+          <li className={classNames}>
+              <span className="list-group-item-label" onClick={this.toggleLike}>{name}</span>
+              <input type="text" className="list-group-item-input" defaultValue={salary + '$'}/>
+              <div className='d-flex justify-content-center align-items-center'>
+                  <button type="button"
+                      className="btn-cookie btn-sm"
+                      onClick={this.onIncrease}>
+                      <i className="fas fa-cookie"></i>
+                  </button>
+
+                  <button type="button"
+                          className="btn-trash btn-sm ">
+                      <i className="fas fa-trash"></i>
+                  </button>
+                  <i className="fas fa-star"></i>
+              </div>
+          </li>
+      )
+    }
 }
 
 export default EmployeesListItem;
