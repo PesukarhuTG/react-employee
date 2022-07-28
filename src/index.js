2fb8d83d0e9b1be8c1cc7e015b9baa2e668ef708
Tatiana <ganzhinovat@yandex.ru>
2022-07-28 10:38:20 +0300

    init: update files

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+//import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+//const elem = <h2>Hello, yoy</h2>;
+//const elem = React.createElement('h2', { className: 'greet'}, 'Hello');
+//const text = new Date();
+const elem = (
+  <div>
+    <h2>Hello: {[1,2,3]}</h2>
+    <input type="text" />
+    <button />
+  </div>
+
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(
+  elem
+);
